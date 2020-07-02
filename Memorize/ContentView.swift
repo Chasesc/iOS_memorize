8e13732975a08a96eb3db187a2e320f413a74c6e
Chase Schelthoff <schelthoff.chase@gmail.com>
2020-07-01 22:41:43 -0400

    basic card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World! wow")
+        ZStack(content: {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👻")
+        })
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
 
