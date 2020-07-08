5583f003fd8622ca8a3b9bc707d5e4300f74174c
Chase Schelthoff <schelthoff.chase@gmail.com>
2020-07-07 22:42:34 -0400

    cleanup

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,30 +14,51 @@ struct EmojiMemoryGameView: View {
     var body: some View {
         HStack {
             ForEach(viewModel.cards) { card in
-                CardView(card: card).onTapGesture {
-                    self.viewModel.choose(card: card)
-                }.aspectRatio(2/3, contentMode: .fit)
+                self.cardView(for: card)
             }
         }
             .padding()
             .foregroundColor(Color.orange)
-        .font(viewModel.cards.count < 5 ? Font.largeTitle : Font.title)
     }
+    
+    func cardView(for card: MemoryGame<String>.Card) -> some View {
+        CardView(card: card).onTapGesture {
+            self.viewModel.choose(card: card)
+        }
+            .aspectRatio(aspectRatio, contentMode: .fit)
+    }
+    
+    let aspectRatio: CGFloat = 2/3
 }
 
 struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: strokeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
+            .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: UI Constants
+    let cornerRadius: CGFloat = 10.0
+    let strokeLineWidth: CGFloat = 3.0
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.height, size.width) * 0.75
     }
 }
 
